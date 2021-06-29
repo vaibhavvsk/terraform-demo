a6cdf32943bad95f40e8b23c8e709b29f11d3a07
Vaibhav Kulkarni <vaibhav.vsk@gmail.com>
2021-06-29 15:02:28 +0530

    Adding .tf configurations

---
+++ create_ec2_instance.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  region     = "ap-south-1"
+}
+# ami-0a05821577033f94d is available in Mumbai reg
+resource "aws_instance" "myec2" {
+   ami = var.ami
+   instance_type = var.instance_type
+}
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "instance_type" {
+    default = "t2.micro"
+    type = string
+}
+variable "ami" {
+    type = string
+    default = "ami-0ad704c126371a549"
+}
